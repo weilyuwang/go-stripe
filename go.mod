60b99ae93250a7c15414e61401a9458d5ffb157e
weilyuwang <weilyuwang@gmail.com>
2022-02-12 13:13:17 -0500

    create stripe paymentIntent from backend service

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/weilyuwang/go-stripe
 
 go 1.17
 
-require github.com/go-chi/chi/v5 v5.0.7 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/stripe/stripe-go/v72 v72.86.0 // indirect
+)
---
+++ internal/card/card.go
@@ -0,0 +1,60 @@
+package card
+
+import (
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/paymentintent"
+)
+
+type Card struct {
+	Secret   string
+	Key      string
+	Currency string
+}
+
+type Transaction struct {
+	TransactionStatusID int
+	Amount              int
+	Currency            string
+	LastFour            string
+	BankReturnCode      string
+}
+
+func (c *Card) Charge(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	return c.createPaymentIntent(currency, amount)
+}
+
+func (c *Card) createPaymentIntent(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	stripe.Key = c.Secret
+
+	// create a payment intent
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(int64(amount)),
+		Currency: stripe.String(currency),
+	}
+
+	// optional: can add metadata to this transaction
+	// params.AddMetadata("key", "value")
+
+	pi, err := paymentintent.New(params)
+	if err != nil {
+		msg := ""
+		if stripeErr, ok := err.(*stripe.Error); ok {
+			msg = cardErrorMessage(stripeErr.Code)
+		}
+		return nil, msg, err
+	}
+	return pi, "", nil
+}
+
+func cardErrorMessage(code stripe.ErrorCode) string {
+	var msg = ""
+	switch code {
+	case stripe.ErrorCodeCardDeclined:
+		msg = "Your card was declined"
+	case stripe.ErrorCodeExpiredCard:
+		msg = "Your card is expired"
+	default:
+		msg = "Your card was declined"
+	}
+	return msg
+}
