b6602d3fa22f59a09a6487e1c7765dd2c0a837a6
weilyuwang <weilyuwang@gmail.com>
2022-02-13 11:27:03 -0500

    generating receipt upon successful payment

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,3 +7,34 @@ func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) 
 		app.errorLog.Println(err)
 	}
 }
+
+func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	// read post data
+	cardHolder := r.Form.Get("cardholder_name")
+	email := r.Form.Get("email")
+	paymentIntent := r.Form.Get("payment_intent")
+	paymentMethod := r.Form.Get("payment_method")
+	paymentAmount := r.Form.Get("payment_amount")
+	paymentCurrency := r.Form.Get("payment_currency")
+
+	data := make(map[string]interface{})
+	data["cardholder"] = cardHolder
+	data["email"] = email
+	data["pi"] = paymentIntent
+	data["pa"] = paymentAmount
+	data["pc"] = paymentCurrency
+	data["pm"] = paymentMethod
+
+	if err := app.renderTemplate(w, r, "succeeded", &templateData{
+		Data: data,
+	}); err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,6 +9,7 @@ func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
+	mux.Post("/payment-succeeded", app.PaymentSucceeded)
 
 	return mux
 }
