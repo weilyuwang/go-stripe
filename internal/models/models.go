efb95b196f872b7e2e1544fb29ec86a26b91b35a
weilyuwang <weilyuwang@gmail.com>
2022-02-19 23:09:48 -0500

    insert transaction db function

--- internal/models/models.go
+++ internal/models/models.go
@@ -114,3 +114,37 @@ func (m *DBModel) GetWidget(id int) (Widget, error) {
 	}
 	return widget, nil
 }
+
+// InsertTransaction inserts a new transaction (txn), and returns its id
+func (m *DBModel) InsertTransaction(txn Transaction) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `
+		insert into transactions
+			(amount, currency, last_four, bank_return_code,
+			transaction_status_id, created_at, updated_at)
+		values (?, ?, ?, ?, ?, ?, ?)
+	`
+
+	result, err := m.DB.ExecContext(ctx, stmt,
+		txn.Amount,
+		txn.Currency,
+		txn.LastFour,
+		txn.BankReturnCode,
+		txn.TransactionStatusID,
+		time.Now(),
+		time.Now(),
+	)
+
+	if err != nil {
+		return 0, nil
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
