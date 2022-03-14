dff0d62bb26d40c21f165434d213d6120c47527a
weilyuwang <weilyuwang@gmail.com>
2022-03-13 23:17:40 -0400

    add helpers to deal with JSON

---
+++ cmd/micro/invoice/helpers.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+// writeJSON writes arbitrary data out as JSON
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	// headers can be length of 0 or 1
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(out)
+
+	return nil
+}
+
+// readJSON reads json from request body into data, We only accept a single json value in the body
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	maxBytes := 1048576
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	// we only allow one entry in the json file
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only have a single JSON value")
+	}
+	return nil
+}
+
+func (app *application) badRequest(w http.ResponseWriter, err error) error {
+	var payload struct {
+		Error   bool   `json:"error"`
+		Message string `json:"message"`
+	}
+
+	payload.Error = true
+	payload.Message = err.Error()
+
+	er := app.writeJSON(w, http.StatusBadRequest, payload)
+	if er != nil {
+		return er
+	}
+	return nil
+}
