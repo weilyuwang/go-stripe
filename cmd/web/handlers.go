3082df7fe6152eeb1b10d2ba25f8fa1f808e6851
weilyuwang <weilyuwang@gmail.com>
2022-03-19 16:41:13 -0400

    set time out limit to 5 seconds

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -261,7 +261,7 @@ func (app *application) callInvoiceMicro(inv Invoice) error {
 		return err
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(out))
 	if err != nil {
